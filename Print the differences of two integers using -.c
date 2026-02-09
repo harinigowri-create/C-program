22d36dfbd5d5f835ac473fca72a25c7fb8bd34f0
harinigowri-create <harinigowrih@gmail.com>
2026-02-09 16:09:41 +0530

    Add files via upload

---
+++ Print the differences of two integers using -.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a=8,b=3;
+    printf("%d",a-b);
+    int c=10,d=5;
+    printf("\n%d",c-d);
+    int e=15,f=10;
+    printf("\n%d",e-f);
+    return 0;
+}
