4ae23ade6e6830cc606f41d16e99a63f773fe83a
harinigowri-create <harinigowrih@gmail.com>
2026-02-09 16:35:32 +0530

    Add files via upload

---
+++ Add three integers and print the result.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a=2,b=3,c=5;
+    printf("%d",a+b+c);
+    int k=1,j=4,l=5;
+    printf("\n%d",k+j+l);
+    int v=3,h=3,s=4;
+    printf("\n%d",v+h+s);
+    
+    return 0;
+}
