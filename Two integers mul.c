790a979696ef1114a073938cfd3223b14a53ee16
harinigowri-create <harinigowrih@gmail.com>
2026-02-09 16:14:50 +0530

    Add files via upload

---
+++ Two integers mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a=4,b=5;
+    printf("%d",a*b);
+    int c=10,d=2;
+    printf("\n%d",c*d);
+    int e=1,f=20;
+    printf("\n%d",e*f);
+    return 0;
+}
+
