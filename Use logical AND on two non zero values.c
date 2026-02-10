36d9e38c626504678ab87cfe523ab2d302228d93
harinigowri-create <harinigowrih@gmail.com>
2026-02-10 11:58:06 +0530

    Add files via upload

---
+++ Use logical AND on two non zero values.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int a=5,b=3;
+    printf("%d",a&&b); 
+    
+    int c=1,d=1;
+    printf("\n%d",c&&d);
+    
+    int e=0,f=5;
+    printf("\n%d", e&&f);
+    return 0;
+}
