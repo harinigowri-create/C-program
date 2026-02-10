25b480b6dd97cce449e1a9a4c802d62e6f482a53
harinigowri-create <harinigowrih@gmail.com>
2026-02-10 12:18:08 +0530

    Add files via upload

---
+++ Assign a value and print it.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+    
+    int a=10;
+    printf("%d",a);
+    
+    int b=5;
+    printf("\n%d",b);
+    
+    int c=0;;
+    printf("\n%d",c);
+    
+    return 0;
+}
