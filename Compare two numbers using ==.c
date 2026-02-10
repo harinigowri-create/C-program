e17d135792e92ab1003f31652eb008572203ec98
harinigowri-create <harinigowrih@gmail.com>
2026-02-10 11:39:08 +0530

    Add files via upload

---
+++ Compare two numbers using ==.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int a=5,b=5;
+    printf("%d",a==b); 
+    
+    int c=3,d=4;
+    printf("\n%d",c==d);
+    
+    int e=0,f=0;
+    printf("\n%d",e==f);
+    return 0;
+}
