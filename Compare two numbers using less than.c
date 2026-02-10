45d5cb90433b54c7e9d341a1484937c9af4be05c
harinigowri-create <harinigowrih@gmail.com>
2026-02-10 11:37:06 +0530

    Add files via upload

---
+++ Compare two numbers using less than.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int a=5,b=10;
+    printf("%d",a<b); 
+    
+    int c=9,d=2;
+    printf("\n%d",c<d);
+    
+    int e=4,f=4;
+    printf("\n%d",e<f);
+    return 0;
+}
