48725614476b0e36dbc81d752748ef9791a185f1
harinigowri-create <harinigowrih@gmail.com>
2026-02-10 11:51:50 +0530

    Add files via upload

---
+++ Compare two numbers using greater than and less than.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int a=5,b=5;
+    printf("%d %d", a>=b , a<=b); 
+    
+    int c=10,d=5;
+    printf("\n%d %d", c>=d , c<=d);
+    
+    int e=3,f=7;
+    printf("\n%d %d", e>=f , e<=f);
+    return 0;
+}
