5cc3898dac5fb3ea82412220e776565460fc187d
harinigowri-create <harinigowrih@gmail.com>
2026-02-10 11:41:02 +0530

    Add files via upload

---
+++ Compare two numbers using !=.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int a=5,b=3;
+    printf("%d",a!=b); 
+    
+    int c=4,d=4;
+    printf("\n%d",c!=d);
+    
+    int e=9,f=1;
+    printf("\n%d",e!=f);
+    return 0;
+}
