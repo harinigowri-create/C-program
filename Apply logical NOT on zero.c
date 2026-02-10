9c43d9421b7a842c57027a7861c2430267d38b84
harinigowri-create <harinigowrih@gmail.com>
2026-02-10 12:05:41 +0530

    Add files via upload

---
+++ Apply logical NOT on zero.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+    
+    int a=0;
+    printf("%d",!a);
+    
+    int b=0;
+    printf("\n%d",!b);
+    
+    int c=0;
+    printf("\n%d",!c);
+    
+    return 0;
+}
