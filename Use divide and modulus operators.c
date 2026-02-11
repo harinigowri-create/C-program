847252cf6030821e0f783b03fcefd897ec5540af
harinigowri-create <harinigowrih@gmail.com>
2026-02-11 11:57:23 +0530

    Add files via upload

---
+++ Use divide and modulus operators.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+ int main() {
+     int a,b;
+     int q,r;
+     
+     scanf("%d %d",&a,&b);
+     q = a;
+     r = a;
+     q/=b;
+     r%=b;
+     printf("\n%d %d", q,r);
+     
+     scanf("%d %d",&a,&b);
+     q = a;
+     r = a;
+     q/=b;
+     r%=b;
+     printf("\n%d %d", q,r);
+     
+     scanf("%d %d",&a,&b);
+     q = a;
+     r = a;
+     q/=b;
+     r%=b;
+     printf("\n%d %d", q,r);
+     
+     return 0;
+     
+ }
