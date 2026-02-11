6c5fcf5329576c0eb88ec9dbdf8302dd6efde490
harinigowri-create <harinigowrih@gmail.com>
2026-02-11 10:03:57 +0530

    Add files via upload

---
+++ Use mul= operator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a=4,b=5;
+    printf("%d",a*=b); 
+    
+    int c=3,d=3;
+    printf("\n%d",c*=d);
+    
+    int e=10,f=2;
+    printf("\n%d",e*=f);
+    return 0;
+}
