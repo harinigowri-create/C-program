0656e75b29365d3606ea73a6cd0ff2cef725e8f4
harinigowri-create <harinigowrih@gmail.com>
2026-02-11 09:56:29 +0530

    Add files via upload

---
+++ Use -= operator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a=10,b=5;
+    printf("%d",a-=b); 
+    
+    int c=20,d=10;
+    printf("\n%d",c-=d);
+    
+    int e=5,f=5;
+    printf("\n%d",e-=f);
+    return 0;
+}
