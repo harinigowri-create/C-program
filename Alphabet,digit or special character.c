1d7be94e996af464cf9a0bf779a5c7d95cb87db2
harinigowri-create <harinigowrih@gmail.com>
2026-02-12 11:21:45 +0530

    Add files via upload

---
+++ Alphabet,digit or special character.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	char ch;
+	scanf("%c",&ch);
+	
+	if((ch>=65 && ch<=90 || ch>=90 && ch<=122)) {
+	    printf("Alphabet");
+	}
+	else if(ch>=48 && ch<=57) {
+	    printf("Digit");
+	}
+	else {
+	    printf("Special character");
+	}
+	return 0;
+}	
