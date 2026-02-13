0b1652af9db3c7e45ab7f4877642951ee7b1ca03
harinigowri-create <harinigowrih@gmail.com>
2026-02-13 12:04:59 +0530

    Add files via upload

---
+++ Multiplication table.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+int main() {
+    int n=5;
+    scanf("%d",&n);
+    for(int i=1; i<=10; i++) {
+        printf("%d ",n * i);
+    }
+    return 0;
+}
+
+#include<stdio.h>
+int main() {
+int n=1;
+scanf("%d",&n);
+for(int i=1; i<=10; i++)  {
+        printf("\n%d ",n * i);
+    } 
+    return 0;
+}
+
+
+#include<stdio.h>
+int main(){
+int n=3;
+    scanf("%d",&n);
+    for(int i=1; i<=10; i++) {
+        printf("\n%d ",n * i);
+    }
+    return 0;
+}
