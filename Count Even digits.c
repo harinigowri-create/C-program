bce8c5ca8846f17a49d8d6cbf7a67d9781260534
harinigowri-create <harinigowrih@gmail.com>
2026-02-16 10:25:56 +0530

    Add files via upload

---
+++ Count Even digits.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+    int n;
+    int count=0;
+    int digit;
+    scanf("%d",&n);
+    
+    
+    for(;n>0;n=n/10) {
+       digit = n % 10;
+       if(digit % 2 == 0){
+           count++;
+       }
+    }    
+    printf("%d",count);
+    
+    return 0;
+}
