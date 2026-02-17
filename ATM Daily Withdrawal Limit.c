1be9479434f0936c8d34b2c814ce6ba5677fe5f4
harinigowri-create <harinigowrih@gmail.com>
2026-02-17 10:07:54 +0530

    Add files via upload

---
+++ ATM Daily Withdrawal Limit.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<math.h>
+
+int main() {
+    int n,amount;
+    int total = 0;
+    
+    scanf("%d",&n);
+    
+    
+    for(int i=0;i<n;i++) {
+        scanf("%d",&amount);
+        total += amount;
+    }
+        
+        if(total > 10000) {
+            printf("Limit Exceeded");
+        }
+        else {
+            printf("Approved");
+        }
+   
+    
+    return 0;
+
+}
+
