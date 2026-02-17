f1c00ab57a8762c53e9d966034d8780a4930cfc4
harinigowri-create <harinigowrih@gmail.com>
2026-02-17 16:11:13 +0530

    Add files via upload

---
+++ Online Sales Target Tracker.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n,i;
+    int revenue;
+    int total = 0;
+    int targetDays = 0;
+  
+    scanf("%d",&n);
+    
+    for(int i=0; i<n; i++) 
+    {
+        scanf("%d",&revenue);
+        total += revenue;
+        
+        if(revenue > 50000) {
+            targetDays++;
+        }
+    }   
+    printf("Total Revenue : %d\n", total);
+    printf("Target Days : %d\n", targetDays);
+   
+    return 0;
+
+}
+
