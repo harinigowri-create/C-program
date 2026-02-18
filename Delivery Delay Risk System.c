406f0fb6a1394f008630c995f803ec89f5ed4442
harinigowri-create <harinigowrih@gmail.com>
2026-02-18 10:34:03 +0530

    Add files via upload

---
+++ Delivery Delay Risk System.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+	int n,delay;
+	int totalDelay;
+	int delayedCount = 0;
+	
+	scanf("%d",&n);
+	
+	for(int i = 0;i < n;i++)
+	{
+	    scanf("%d",&delay);
+	    totalDelay += delay;
+	    
+	    if(delay > 30)
+	    {
+	        delayedCount++;
+	    }
+	}
+    printf("Total Delay : %d\n",totalDelay);
+    printf("Delayed Deliveries  : %d\n",delayedCount);
+    
+    return 0;
+}
+
