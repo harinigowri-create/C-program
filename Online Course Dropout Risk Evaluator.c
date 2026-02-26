639e0f7a41ebec9a2ef0fe666fe188a0163ce49b
harinigowri-create <harinigowrih@gmail.com>
2026-02-26 15:25:30 +0530

    Add files via upload

---
+++ Online Course Dropout Risk Evaluator.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int i = 0;
+    int hours;
+    int inactiveWeeks = 0;
+
+    while (i < N) {
+        scanf("%d", &hours);
+
+        if (hours == 0) {
+            inactiveWeeks++;
+        }
+
+        i++;
+    }
+
+    printf("Inactive Weeks: %d\n", inactiveWeeks);
+
+    if (inactiveWeeks >= 3) {
+        printf("Risk Status: High\n");
+    } else {
+        printf("Risk Status: Low\n");
+    }
+
+    return 0;
+}
