7b1bd64f023ea76cc238b035ad72475cf0734b16
harinigowri-create <harinigowrih@gmail.com>
2026-03-01 21:11:01 +0530

    Update and rename Nested Loop-11.c.txt to Nested Loop-11.c

--- Nested Loop-11.c
+++ Nested Loop-11.c
@@ -5,11 +5,12 @@ int main() {
         for(i = 1; i <= 5; i++) {
 
                 for(j = 1; j <= i; j++) {
-                        printf(" *");
+                        printf("*");
                 }
                 
                 printf("\n");
 
       }
       return 0;
+
 }
