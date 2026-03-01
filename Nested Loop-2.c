d46a1329a998441d4630a2e31474e9f008202145
harinigowri-create <harinigowrih@gmail.com>
2026-03-01 20:16:32 +0530

    Fix formatting and syntax in Nested Loop-2.c

--- Nested Loop-2.c
+++ Nested Loop-2.c
@@ -3,11 +3,12 @@ int main() {
     int i,j;
 
     for(i = 1;i <= 5;i++) {
-           for(j = 1;j <= i;j++) {
-                  printf("%d",i);
-           }
-           printf("\n");
+        for(j = 1;j <= i;j++) {
+            printf("%d",i);
+        }
+        printf("\n");
 
    }
    return 0;
 }
+
