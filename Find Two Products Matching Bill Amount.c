47786a5d69f84974089518d578fb303b96b63440
harinigowri-create <harinigowrih@gmail.com>
2026-03-05 10:57:21 +0530

    Add files via upload

---
+++ Find Two Products Matching Bill Amount.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, target;
+    scanf("%d", &n);
+
+    int prices[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &prices[i]);
+    }
+
+    scanf("%d", &target);
+
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(prices[i] + prices[j] == target) {
+                printf("%d %d\n", prices[i], prices[j]);
+                return 0;
+            }
+        }
+    }
+
+    printf("No pair\n");
+    return 0;
+}
