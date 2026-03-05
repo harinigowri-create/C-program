a94f59cf4630b112564f36b13f5804ff223b26bf
harinigowri-create <harinigowrih@gmail.com>
2026-03-05 11:42:35 +0530

    Add files via upload

---
+++ Find Cheapest Valid Product Price.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int prices[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &prices[i]);
+    }
+
+    int min_price = INT_MAX;
+    for(int i = 0; i < n; i++) {
+        if(prices[i] > 0 && prices[i] < min_price) {
+            min_price = prices[i];
+        }
+    }
+
+    if(min_price == INT_MAX) {
+        printf("No positive\n");
+    } else {
+        printf("%d\n", min_price);
+    }
+
+    return 0;
+}
