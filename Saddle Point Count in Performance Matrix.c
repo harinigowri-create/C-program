a52e111eacf3e2a46e26e30b64113165ea2b6acc
harinigowri-create <harinigowrih@gmail.com>
2026-03-09 10:10:39 +0530

    Add files via upload

---
+++ Saddle Point Count in Performance Matrix.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int R, C;
+    scanf("%d %d", &R, &C);
+
+    int a[R][C];
+
+    for(int i = 0; i < R; i++)
+        for(int j = 0; j < C; j++)
+            scanf("%d", &a[i][j]);
+
+    int count = 0;
+
+    for(int i = 0; i < R; i++) {
+        for(int j = 0; j < C; j++) {
+
+            int isRowMin = 1, isColMax = 1;
+
+            // check row minimum
+            for(int k = 0; k < C; k++) {
+                if(a[i][k] < a[i][j]) {
+                    isRowMin = 0;
+                    break;
+                }
+            }
+
+            // check column maximum
+            for(int k = 0; k < R; k++) {
+                if(a[k][j] > a[i][j]) {
+                    isColMax = 0;
+                    break;
+                }
+            }
+
+            if(isRowMin && isColMax)
+                count++;
+        }
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
