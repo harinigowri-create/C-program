61cbb7543da0963da6ac2969f7aa8c7c01badd19
harinigowri-create <harinigowrih@gmail.com>
2026-03-17 15:31:08 +0530

    Add files via upload

---
+++ Reverse a Number Using a Function.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int reverseNumber(int n) {
+    int rev = 0;
+    
+    while(n!=0) {
+        int digit = n % 10;
+        rev =rev *10 + digit;
+        n = n/10;
+    }
+    return rev;
+}
+int main() {
+    int n;
+    scanf("%d",&n);
+    int result = reverseNumber(n);
+    printf("%d",result);
+    return 0;
+}
